f4b9399a207aa52c58c9e8c55d27391606d2f62d
Hobs Towler <91224707+hobstowler@users.noreply.github.com>
2024-12-10 13:51:46 -0500

    sentinment

--- sentiment.py
+++ sentiment.py
@@ -1,12 +1,26 @@
+import torch.cuda
 from transformers import pipeline
 
 class SentimentAnalyzer:
-    def __init__(self, data, model_name=None):
+    def __init__(self, data: dict, model_name=None, device=None):
+        if device is None:
+            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+            print(device)
+
         if model_name is None:
-            self.pipeline = pipeline('sentiment-analysis')
+            self.pipeline = pipeline('sentiment-analysis', device=device)
         else:
             self.pipeline = pipeline(model=model_name)
 
         self.raw_data = data
 
-    def run
+    def run(self):
+        print(self._get_text())
+
+    def _get_text(self) -> list:
+        return [value for key, value in self.raw_data.items() if key == 'text']
+
+
+if __name__ == '__main__':
+    s = SentimentAnalyzer(None, device='cuda')
+    s.run()
