d22cbb2e737d690d3e2c3f9e07abb82e11426e23
Hobs Towler <91224707+hobstowler@users.noreply.github.com>
2024-12-11 08:49:02 -0500

    sentinment final

--- sentiment.py
+++ sentiment.py
@@ -1,26 +1,141 @@
+import json
+import os
+import random
+
 import torch.cuda
+import numpy as np
+
+from torch.utils.hipify.hipify_python import InputError
 from transformers import pipeline
+from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
 
 class SentimentAnalyzer:
-    def __init__(self, data: dict, model_name=None, device=None):
+    hugging_face = False
+    text_blob = False
+    vader = False
+
+    def __init__(self, file_name: str, sample_size=10000, hugging_face: any = False, text_blob=False, vader=False, device=None):
         if device is None:
             device = 'cuda' if torch.cuda.is_available() else 'cpu'
-            print(device)
+            print(f'Using {device}.')
 
-        if model_name is None:
-            self.pipeline = pipeline('sentiment-analysis', device=device)
-        else:
-            self.pipeline = pipeline(model=model_name)
+        if not hugging_face and not text_blob and not vader:
+            raise InputError('Model not specified')
 
-        self.raw_data = data
+        if hugging_face:
+            self.hugging_face = hugging_face
+            try:
+                self.pipeline = pipeline('sentiment-analysis', model=hugging_face, device=device, truncation=True)
+            except AssertionError as e:
+                print(e)
+                print(f'Using cpu instead.')
+                self.pipeline = pipeline('sentiment-analysis', model=hugging_face, device='cpu', truncation=True)
+
+        if text_blob:
+            self.text_blob = text_blob
+            self.model_name = 'textblob'
+
+        if vader:
+            self.vader = vader
+            self.model_name = 'vader'
+
+        self.file_name = file_name
+        self.sample_size = sample_size
 
     def run(self):
-        print(self._get_text())
+        # load the file and sample a subset of data
+        sample_data = self._load_file()
 
-    def _get_text(self) -> list:
-        return [value for key, value in self.raw_data.items() if key == 'text']
+        # retrieve just the text for analysis
+        text = self._get_text(sample_data)
+
+        # run the analysis
+        sentiment = self._analyze_sentiment(text)
+        combined_data = [{**z1, **z2} for z1, z2 in zip(sample_data, sentiment)]
+
+        # sort into buckets based on rating
+        buckets = self._sort_sentiment_by_rating(combined_data)
+        buckets.update({'all': combined_data})
+
+        #output intermediate results
+        filename = os.path.join(os.getcwd(), 'analyzed', f'{self.file_name.split(".")[0]}_{self.model_name.replace("/", "_")}.json')
+        self._output_to_file(filename, buckets)
+
+        performance = {k: self._calculate_error(k, bucket) for k, bucket in buckets.items()}
+        filename = os.path.join(os.getcwd(), 'analyzed', f'{self.file_name.split(".")[0]}_{self.model_name.replace("/", "_")}_results.json')
+        self._output_to_file(filename, performance)
+
+    def _calculate_error(self, actual_score: float, bucket: list):
+        scores = np.array(list(b['score'] for b in bucket))
+        if actual_score == 'all':
+            actual_scores = np.array(list(b['rating'] for b in bucket))
+        else:
+            actual_scores = np.full(scores.shape, float(actual_score))
+
+        mae = np.mean(np.abs(scores - actual_scores))
+        rmse = np.sqrt(np.mean((scores - actual_scores) ** 2))
+        acc = np.mean(np.isclose(np.round(scores, 0), actual_scores))
+
+        return {'size': len(bucket), 'mae': round(float(mae), 4), 'rmse': round(float(rmse), 4), 'acc': round(float(acc), 4)}
+
+    def _load_file(self):
+        data = json.load(open(fr'processed\{self.file_name}'))
+        sample_data = random.sample(data, self.sample_size)
+
+        return sample_data
+
+    def _get_text(self, sample_data) -> list:
+        return [review.get('text', '') for review in sample_data]
+
+    def _analyze_sentiment(self, text: list):
+        if self.hugging_face:
+            return self.pipeline(text)
+        elif self.text_blob:
+            sentiment = []
+            for review in text:
+                score = (TextBlob(review).sentiment.polarity + 1) / 2 * 5
+                if score < 2.5:
+                    label = 'NEGATIVE'
+                elif score > 2.5:
+                    label = 'POSITIVE'
+                else:
+                    label = 'NEUTRAL'
+                sentiment.append({'score': score, 'label': label})
+            return sentiment
+        elif self.vader:
+            sentiment = []
+            analyzer = SentimentIntensityAnalyzer()
+            for review in text:
+                score = (analyzer.polarity_scores(review)['compound'] + 1) / 2 * 5
+                if score < 2.5:
+                    label = 'NEGATIVE'
+                elif score > 2.5:
+                    label = 'POSITIVE'
+                else:
+                    label = 'NEUTRAL'
+                sentiment.append({'score': score, 'label': label})
+            return sentiment
+
+    def _sort_sentiment_by_rating(self, combined_data):
+        buckets = {1.0: [], 2.0: [], 3.0: [], 4.0: [], 5.0: []}
+
+        for review in combined_data:
+            buckets[review['rating']].append({k: v for k, v in review.items() if k not in ['rating', 'text']})
+
+        return buckets
+
+    def _output_to_file(self, filename, buckets):
+        with open(filename, 'w') as f:
+            json.dump(buckets, f)
 
 
 if __name__ == '__main__':
-    s = SentimentAnalyzer(None, device='cuda')
+    # data = json.load(open(r'processed\Digital_Music.json'))
+    # s = SentimentAnalyzer(file_name='Digital_Music.json', hugging_face='siebert/sentiment-roberta-large-english', device='cuda')
+    s = SentimentAnalyzer(file_name='Digital_Music.json', text_blob=True, device='cuda')
+    # s = SentimentAnalyzer(file_name='Digital_Music.json', vader=True, device='cuda')
     s.run()
+    # analyzer = SentimentIntensityAnalyzer()
+    # print(analyzer.polarity_scores("If i had a dollar for how many times I have played this cd and how many times I have asked Alexa to play it, I would be rich. Love this singer along with the Black Pumas. Finding a lot of new music that I like a lot on amazon. Try new things."))
